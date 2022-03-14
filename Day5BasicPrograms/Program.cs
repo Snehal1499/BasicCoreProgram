26b92ee477c3d8cf1a19e4a86029631f7030d02c
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-14 13:30:58 +0530

    Added Even Or Odd program

--- Day5BasicPrograms/Program.cs
+++ Day5BasicPrograms/Program.cs
@@ -4,9 +4,34 @@ namespace Day5BasicPrograms
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public class Even_Or_Odd
         {
-            Console.WriteLine("Welcom To Basic Core Programs");
+            public void EvenOrOdd(int number)
+            {
+
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine($"Given Number {number} is Even");
+
+                }
+                else
+                {
+                    Console.WriteLine($"Given Number {number} is Odd");
+                }
+            }
+
+        }
+        internal class Programs
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter Numbers ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Even_Or_Odd eo = new Even_Or_Odd();
+                eo.EvenOrOdd(number);
+
+            }
         }
     }
 }
+
