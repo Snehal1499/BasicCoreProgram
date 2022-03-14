7238d0d5b72a6bc0251f23dcff21b7ccea4ac368
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-14 13:26:15 +0530

    Added Swap Two Number Programe

--- Day5BasicPrograms/Program.cs
+++ Day5BasicPrograms/Program.cs
@@ -4,9 +4,36 @@ namespace Day5BasicPrograms
 {
     internal class Program
     {
-        static void Main(string[] args)
+        
+         
+            public class SwapNumber
         {
-            Console.WriteLine("Welcom To Basic Core Programs");
+            int temp = 0;
+            public void Swapping(int number1, int number2)
+            {
+                temp = number1;
+                number1 = number2;
+                number2 = temp;
+                Console.WriteLine($"After Swapping number 1 : {number1} number 2 : {number2}");
+            }
+        
+        }
+        internal class Programs
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter number1 and number2");
+                int number1 = Convert.ToInt32(Console.ReadLine());
+                int number2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Before Swapping number1 : {number1} number 2 : {number2}");
+                SwapNumber swap = new SwapNumber();
+                swap.Swapping(number1, number2);
+            }
         }
     }
 }
+
+
+
+
+
