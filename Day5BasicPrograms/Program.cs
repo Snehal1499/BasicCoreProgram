d1afb1e6d9cf9b5af5d6a2ca55af6d7ce4c5cf04
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-14 13:34:24 +0530

    Added Alphate is vowel or Consonat

--- Day5BasicPrograms/Program.cs
+++ Day5BasicPrograms/Program.cs
@@ -4,9 +4,32 @@ namespace Day5BasicPrograms
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public class VowelOrConsonant
         {
-            Console.WriteLine("Welcom To Basic Core Programs");
+            public void Vowel_OR_Consonant(char letter)
+            {
+                if (letter == 'a' || letter == 'A' || letter == 'e' || letter == 'E' || letter == 'i' || letter == 'I' || letter == 'o' || letter == 'O' || letter == 'u' || letter == 'U')
+                {
+                    Console.WriteLine($"Given letter : {letter} is Vowel");
+                }
+                else
+                {
+                    Console.WriteLine($"Given letter : {letter} is Consonant");
+                }
+            }
+        }
+        internal class Programs
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter a Char Value");
+                char letter = Convert.ToChar(Console.ReadLine());
+                VowelOrConsonant voc = new VowelOrConsonant();
+                voc.Vowel_OR_Consonant(letter);
+            }
         }
     }
 }
+
+
+
