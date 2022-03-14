d260858e3c3f6962ea4e21a72e180b155158876c
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-14 12:47:44 +0530

    Added Harmonic Number Programe

--- Day5BasicPrograms/Program.cs
+++ Day5BasicPrograms/Program.cs
@@ -4,9 +4,30 @@ namespace Day5BasicPrograms
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public class Harmonic
         {
-            Console.WriteLine("Welcom To Basic Core Programs");
+            public void HarmonicNum(int number)
+            {
+                double result = 0;
+                for (int i = 1; i <= number; i++)
+                {
+                    result = (result + 1) / i;
+                }
+                Console.WriteLine($"Harmonic Number of given number is :{result}");
+            }
+        }
+        internal class Programs
+        {
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Enter Number :");
+                Harmonic h = new Harmonic();
+                int number = Convert.ToInt32(Console.ReadLine());
+                h.HarmonicNum(number);
+            }
         }
     }
 }
+
+
+
