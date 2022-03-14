ffb185f329ee37ea94ebca744def7b4345305f9f
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-14 12:55:45 +0530

    Added Quotient and Remainder Programe

--- Day5BasicPrograms/Program.cs
+++ Day5BasicPrograms/Program.cs
@@ -4,9 +4,27 @@ namespace Day5BasicPrograms
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public class Quoitent_And_Remainder
         {
-            Console.WriteLine("Welcom To Basic Core Programs");
+            public void Quoitent_and_Remainder(double number)
+            {
+                double result1 = number % 2;
+                double result2 = number / 2;
+                Console.WriteLine($"Remainder of given number is : {result1}");
+                Console.WriteLine($"Quoitent of given number is : {result2}");
+            }
+
+        }
+        internal class Programs
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter Number");
+                double number = Convert.ToDouble(Console.ReadLine());
+                Quoitent_And_Remainder qandr = new Quoitent_And_Remainder();
+                qandr.Quoitent_and_Remainder(number);
+
+            }
         }
     }
 }
